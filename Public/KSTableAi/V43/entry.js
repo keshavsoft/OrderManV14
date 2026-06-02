4b5a3ce3b848da5d5d59f77e2b6884c5916964f9
Keshav Nalam <nknnkumar@yahoo.com>
2026-06-02 22:43:10 +0530

    del

--- Public/KSTableAi/V43/entry.js
+++
@@ -1,8 +0,0 @@
-import { KSAiTable } from "./ai.js";
-
-export const initShowTable = async (cfg) => {
-    // debugger;
-    const table = new KSAiTable(cfg);
-    await table.initShowTable();   // ✅ THIS is missing
-    return table;
-};
